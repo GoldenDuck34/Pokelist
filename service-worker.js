07589969ee01ef0df736b8f371e80747dc8c53f3
GoldenDuck34 <aidenberr@gmail.com>
2025-12-11 17:05:04 -0500

    Removed offline suppport It would be pretty useless anyway, since there wouldn't be images

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,13 @@
-const CACHE_NAME = 'pwa-cache-v1';
-const urlsToCache = [
-  './index.html',
-  './alola.js',
-  './bd-sp.json',
-  './black-white.html',
-  './black2-white2.html',
-  './brilliantdiamond-shiningpearl.html',
-  './circle.png',
-  './crystal.html',
-  './diamond-pearl.html',
-  './double_checkbox.css',
-  './emerald.html',
-'./favicon.png',
-  './favicon-19.png',
-  './favicon-50.png',
-  './firered-leafgreen.html',
-  './gold-silver.html',
-  './heartgold-soulsilver.html',
-  './home.css',
-  './home.js',
-  './letsgo.html',
-  './letsgo.json',
-  './manifest.json',
-  './omegaruby-alphasapphire.html',
-  './pla.html',
-  './pla.json',
-  './platinum.html',
-  './plza.html',
-  './plza.json',
-  './red-blue.html',
-  './ruby-sapphire.html',
-  './scarlet-violet.html',
-  './script.js',
-  './single_checkbox.css',
-  './style.css',
-  './sun-moon.html',
-  './sv.js',
-  './sv.json',
-  './sword-shield.js',
-  './sword-shield.json',
-  './sword-shield.html',
-  './ultra-alola.js',
-  './ultra-sun-ultra-moon.html',
-  'x-y.html',
-  'xy.js',
-  'yellow.html'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+// Disable all caching
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+// Always fetch from network, never cache
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
